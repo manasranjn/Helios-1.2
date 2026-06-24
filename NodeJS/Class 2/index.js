e1c8971532c21334399041407c19ebbc09d199dc
Manas Ranjan Bera <manasbera484@gmail.com>
2026-06-24 13:17:01 +0530

    fs

---
+++ NodeJS/Class 2/index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+// console.log(path);
+
+// console.log(path.basename("/Class 2/app.js"))
+// console.log(path.dirname("/Class 2/app.js"));
+// console.log(path.extname("/Class 2/app.js"));
+// console.log(path.resolve('app.js'));
+// console.log(path.isAbsolute('C:/Users/91801/Desktop/Web Bocket Classes/Helios 1.2/NodeJS/Class 2/app.js'));
+
+console.log(__dirname);
+console.log(__filename);
