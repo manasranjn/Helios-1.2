05000393d7b19db5666031fa4bb393e63c4d8eb4
Manas Ranjan Bera <manasbera484@gmail.com>
2026-06-29 13:08:09 +0530

    routes

--- ExpressJS/first-project/server.js
+++ ExpressJS/first-project/server.js
@@ -2,6 +2,26 @@ const express = require('express')
 const PORT = 3000
 const app = express()
 
+app.get('/', (req, res) => {
+    // console.log(req.method, req.path);
+    res.send("This is Home page")
+})
+
+app.get('/about', (req, res) => {
+    res.send("This is about page")
+})
+
+app.post('/about', (req, res) => {
+    res.send("About page POST request...")
+})
+
+app.put('/about', (req, res) => {
+    res.send("About edit request")
+})
+
+app.delete('/about', (req, res) => {
+    res.send("Delete request")
+})
 
 
 app.listen(PORT, () => {
