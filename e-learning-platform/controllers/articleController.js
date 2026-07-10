20065cfa26d38e4949bc3db1f2e127c406572b96
Manas Ranjan Bera <manasbera484@gmail.com>
2026-07-10 13:12:13 +0530

    project

--- e-learning-platform/controllers/articleController.js
+++ e-learning-platform/controllers/articleController.js
@@ -1,5 +1,6 @@
 const Article = require('../models/article')
 
+//! Create Article
 exports.createArticle = async (req, res) => {
     try {
         const { title, description, images, author, category } = req.body
@@ -30,6 +31,124 @@ exports.createArticle = async (req, res) => {
         return res.status(500).json({
             success: false,
             message: error.message
+        })
+    }
+}
+
+//! Get All Articles
+exports.getAllArticles = async (req, res) => {
+    try {
+        const articles = await Article.find().sort({ createdAt: -1 })
+
+        return res.status(200).json({
+            success: true,
+            message: "All articles fetched",
+            count: articles.length,
+            data: articles
+
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        })
+    }
+}
+
+//! Get Article by ID
+exports.getArticleById = async (req, res) => {
+    try {
+        const id = req.params.id
+
+        const article = await Article.findById(id)
+
+        if (!article) {
+            return res.status(404).json({
+                success: false,
+                message: "Article Not Found",
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Article Found",
+            data: article
+        })
+
+    } catch {
+        return res.status(500).json({
+            success: false,
+            message: "Article Not Found",
+        })
+    }
+}
+
+//! Update Article
+exports.updateArticle = async (req, res) => {
+    try {
+        const id = req.params.id
+
+        const { title, description, image, author, category } = req.body
+
+        const article = await Article.findById(id)
+
+        if (!article) {
+            return res.status(404).json({
+                success: false,
+                message: "Article Not Found",
+            })
+        }
+
+        //? Update Fields
+        article.title = title || article.title
+        article.description = description || article.description
+        article.image = image || article.image
+        article.author = author || article.author
+        article.category = category || article.category
+
+        await article.save()
+
+        return res.status(200).json({
+            success: true,
+            message: "Article Updated Successfully",
+            data: article
+        })
+
+
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
+
+//! delete Article
+exports.deleteArticle = async (req, res) => {
+    try {
+        const id = req.params.id
+
+        const article = await Article.findById(id)
+
+        if (!article) {
+            return res.status(404).json({
+                success: false,
+                message: "Article Not Found",
+            })
+        }
+
+        await article.deleteOne()
+
+        return res.status(200).json({
+            success: true,
+            message: "Article Deleted successfully",
+            data: article
+        })
+
+    } catch {
+        return res.status(500).json({
+            success: false,
+            message: "Article Not Found",
         })
     }
 }
--- e-learning-platform/routes/articleRoute.js
+++ e-learning-platform/routes/articleRoute.js
@@ -1,7 +1,11 @@
-const { createArticle } = require('../controllers/articleController')
+const { createArticle, getAllArticles, getArticleById, updateArticle, deleteArticle } = require('../controllers/articleController')
 const express = require('express')
 const router = express.Router()
 
 router.post('/', createArticle)
+router.get('/', getAllArticles)
+router.get('/:id', getArticleById)
+router.put('/:id', updateArticle)
+router.delete('/:id', deleteArticle)
 
 module.exports = router
